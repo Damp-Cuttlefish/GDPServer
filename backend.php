1f1ca6240f5b8ff880ee7c5603cbaaa20a240ccf
Damp-Cuttlefish <tiddles.barker@googlemail.com>
2015-12-01 12:49:35 +0000

    Replace placeholder with prototype code

--- backend.php
+++ backend.php
@@ -1,1 +1,33 @@
-Some text
+<html>
+
+
+<html>
+<body>
+<?php if (array_key_exists('submit', $_POST)): ?>
+Data From Sensor #<?php echo $_POST["uid"]; ?><br>
+
+Level: <?php echo $_POST["level"]; ?><br>
+Max Level: <?php echo $_POST["levelmax"]; ?> <br>
+Fill Percentage: <?php echo 
+100*(intval($_POST["level"])/intval($_POS$
+Battery Level: <?php echo $_POST["battlevel"]; ?><br>
+<?php if (array_key_exists('submit', $_POST)): ?>
+
+<?php else: ?>
+
+<form action="/public/test2.php" method="post">
+Bin Level: <input type="text" name="level"><br>
+Bin Level Max: <input type="text" name="levelmax"><br>
+Battery Level: <input type="text" name="battlevel"><br>
+UID: <input type="text" name="uid"><br>
+
+<input type="submit" name="submit">
+</form>
+
+<?php endif ?>
+
+</body>
+</html>
+
+
+
