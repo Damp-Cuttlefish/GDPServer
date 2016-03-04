5767c966c8fb59b5641271c2ff047d42ae80caab
YWAS <tiddles.barker@googlemail.com>
2016-03-04 12:43:14 +0100

    Completed bin addition, removal and editing. Image upload not yet supported

--- edit.php
+++ edit.php
@@ -2,18 +2,22 @@
 <?php
     include 'lib/accesscheck.php';
     include 'lib/dbopen.php';
-    if (isset($_POST['confirm'])){
-        if ($_GET['uid'] == 'new'){
-            $sql = "INSERT INTO bins VALUES ('".$_POST['uid']."', '".$_POST['name']."', '".$_POST['description']."', '".$_POST['location']."', '".$_POST['csa']."', '".$_SESSION['usr']."', 'OK', '".$_POST['types']."', '".$_POST['groups']."', 'resources/bins/".$_POST['uid'].".jpg')";
-        }
-        else{
-            $uid = $_GET['uid'];
-            $sql = "INSERT INTO users VALUES ('acc-".guidv4(openssl_random_pseudo_bytes(16))."', '".$_POST['username']."', '".password_hash($_POST['pass'], PASSWORD_DEFAULT)."', '".$_POST['email']."', ' ' )";
-        }
+    if (isset($_POST['confirm']))
+             $sql = "REPLACE INTO bins VALUES ('".$_POST['uid']."', '".$_POST['name']."', '".$_POST['description']."', '".$_POST['location']."', '".$_POST['csa']."', '".$_SESSION['usr']."', 'OK', '".$_POST['types']."', '".$_POST['groups']."', 'resources/bins/".$_POST['uid'].".jpg');";
+    elseif (isset($_POST['delete'])){
+        $sql = "DELETE FROM bins WHERE UID = '".$_POST['uid']."';";
+         header('Location: bins.php');
+    }
+    else
+        $uid = $_GET['uid'];
+//    echo $sql;
+    $ret= $db->exec($sql);
 
-        $ret= $db->exec($sql);
-        
-    }  
+    if ($_GET['uid'] == 'new' && isset($_POST['uid']))
+                 header('Location: edit.php?uid='.$_POST['uid']);     
+                     
+    
+  
 
 ?>
 <head>
@@ -40,7 +44,8 @@
         <h3>
 
         <?php if ($_GET['uid'] == 'new'): ?>
-            <form action="edit.php?uid=<?php echo $_GET['uid'] ?>" method="post" id="editform">
+            
+            <form action="edit.php?uid=new" method="post" id="editform">
                         <table>
                             <tr><td>Bin ID: </td><td><input type="text" name="uid" rows="10" value=""></td></tr>
                             <tr><td>Name: </td><td><input type="text" rows="10" name="name" value=""></td></tr>
@@ -54,6 +59,8 @@
 <input type="file" name="image" id="editform" accept="image/*"></td><td><input value="Add" name="confirm" class="lpbutton" type="submit"> <input value="Cancel" class="lpbutton" name="cancel" type="submit"></td></tr>
                         </table>
                     </form>
+                    </h3>
+                    </div>
 
 
         <?php else:
@@ -62,24 +69,23 @@
                 $row = $ret->fetchArray(SQLITE3_ASSOC)                          
         ?>
                     
-                    <form action="edit.php?uid=<?php echo $_GET['uid'] ?>" method="post" id="editform">
+                    <form action="edit.php?uid=<?php echo $_GET['uid'] ?>" method="post" id="editform"><input type ="hidden" name="uid" value="<?php echo $row['uid']; ?>">
                         <table>
-                            <tr><td>Name: </td><td><input type="text" rows="10" value="<?php echo $row['name'];?>"></td></tr>
-                            <tr><td>Description:</td><td> <textarea form="editform"><?php echo $row['description'];?></textarea></td></tr>
-                            <tr><td>Location:</td><td> <input type="text" rows="10" value="<?php echo $row['location'];?>"></td></tr>
-                            <tr><td>Address:</td><td> <textarea form="editform">Broken until I get a geocoding API key</textarea></td></tr>
-                            <tr><td>Groups:</td><td> <input type="text" alt="comma seperated" rows="10" value="<?php echo $row['groups'];?>"></td></tr>
-                            <tr><td>Types:</td><td> <input type="text" rows="10" value="<?php echo $row['type'];?>"></td></tr>
-                            <tr><td>Average Area:</td><td> <input type="number" rows="10" value="<?php echo $row['csa'];?>">cm<sup>2</sup></td></tr>
+                            <tr><td>Name: </td><td><input type="text" name="name" rows="10" value="<?php echo $row['name'];?>"></td></tr>
+                            <tr><td>Description:</td><td> <textarea name="description" form="editform"><?php echo $row['description'];?></textarea></td></tr>
+                            <tr><td>Location:</td><td> <input type="text" name="location" rows="10" value="<?php echo $row['location'];?>"></td></tr>
+                            <tr><td>Address:</td><td> <textarea name="address" form="editform">Broken until I get a geocoding API key</textarea></td></tr>
+                            <tr><td>Groups:</td><td> <input type="text" name="groups" alt="comma seperated" rows="10" value="<?php echo $row['groups'];?>"></td></tr>
+                            <tr><td>Types:</td><td> <input type="text" name="types" rows="10" value="<?php echo $row['type'];?>"></td></tr>
+                            <tr><td>Average Area:</td><td> <input type="number" name="csa" rows="10" value="<?php echo $row['csa'];?>">cm<sup>2</sup></td></tr>
                             <tr><td>
-<input type="file" name="image" id="editform" accept="image/*"></td><td><input value="Confirm Edits" name="confirm" class="lpbutton" type="submit"> <input value="Cancel" class="lpbutton" name="cancel" type="submit"></td></tr>
+<input type="file" name="image" id="editform" accept="image/*"></td><td><input value="Confirm Edits" name="confirm" class="lpbutton" type="submit"> <input value="Cancel" class="lpbutton" name="cancel" type="submit"> <input value="Delete" class="lpbutton" name="delete" type="submit"></td></tr>
                         </table>
                     </form>
-        <?php endif;?>
                     </h3>
                     </div>
                     <div style="float:left;" ><img src="<?php echo $row['image_path'];?>" style="width:200px; height:280px; padding:10px;"></div>
-            
+            <?php endif;?>
         </p>
        
        
